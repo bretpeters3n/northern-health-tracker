a3d6108ddffd551ae40a2047e36fa1a568f29cb1
Bret Petersen <bretpetersen82@gmail.com>
2021-11-02 13:59:16 -0500

    homepage route now working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,9 @@
 const router = require("express").Router();
 const { User } = require("../models");
-const withAuth = require("../utils/auth");
+// const withAuth = require("../utils/auth");
 
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render("homepage", {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", withAuth, async (req, res) => {
+console.log("sdfasdfasd");
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
@@ -27,6 +12,24 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+router.get("*", (req, res) => {
+  console.log("asdfasdfasdf");
+  //   try {
+  //     const userData = await User.findAll();
+
+  //     const users = userData.map((project) => project.get({ plain: true }));
+  //     console.log("sadfdsfasdf" + users);
+  //     // res.json(users);
+  //     res.render("homepage", {
+  //       users,
+  //       //   logged_in: req.session.logged_in,
+  //     });
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  res.render("homepage");
 });
 
 module.exports = router;
