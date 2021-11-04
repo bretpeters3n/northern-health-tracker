2cf24bbc4f0861ab377b01ddb66516381ac678bf
Cole Enyart <letchro@gmail.com>
2021-11-04 09:38:41 -0500

    Added user_id session id

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,11 @@
+let session ='<%= Session["VariableName"]%>';
+/* db.query('SELECT sid FROM sessions') */
+
 const newFormHandler = async function (event) {
   event.preventDefault();
 
   const data = {
+    user_id: '<%= Session["VariableName"]%>',
     day: document.querySelector('#day').value.trim(),
     calorie: document.querySelector('#calorie').value.trim(),
     exercise: document.querySelector('#exercise').value.trim(),
@@ -9,13 +13,34 @@ const newFormHandler = async function (event) {
     water: document.querySelector('#water').value.trim(),
   }
 
-  await fetch(`/api/form`, {
+  const response = await fetch(`/api/form`, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' },
   });
+
+  if (response.ok) {
+    window.alert("New Day created!");
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to add Day');
+  }
 };
 
-document
-  .querySelector('.log-form')
-  .addEventListener('submit', newFormHandler);
+const delButtonHandler = async function (event) {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/form/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to delete project");
+    }
+  }
+};
+
+document.querySelector('.log-form').addEventListener('submit', newFormHandler);
