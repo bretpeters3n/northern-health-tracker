7c049237aae3583c39abaa852ec64e1ceffad40b
Bret Petersen <bretpetersen82@gmail.com>
2021-11-07 18:08:11 -0600

    Added 4 separate graphs. Adding real data next

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,13 +20,13 @@ router.get("/form", withAuth, async (req, res) => {
     const logsData = await Logs.findAll({
       where: {
         user_id: req.session.user_id,
-      }
+      },
     });
     const aa = logsData.map((v) => v.get({ plain: true }));
 
     res.render("form", { logged_in: req.session.logged_in, logs: aa });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 });
 
@@ -51,19 +51,58 @@ router.get("/profile", async (req, res) => {
     const userId = req.session.user_id;
     console.log("This is the signed in users id: " + userId);
 
+    // gather user data for logged in User
     const userData = await Logs.findAll({
       where: {
         user_id: userId,
       },
-      order: [['day', 'DESC']],
-      limit: 3,
+      // order: [['day', 'DESC']],
+      // limit: 3,
+      attributes: ["calorie", "exercise", "sleep", "water"],
     });
+    const loggedInUserData = await userData.map((post) =>
+      post.get({ plain: true })
+    );
+    console.log(loggedInUserData);
+    // END - gather user data for logged in User ^
 
-    // const thisUserData = userData.map((post) => post.get({ plain: true }));
-    console.log(userData);
+    // Find average calories for User
+    const user_calorie = await loggedInUserData.map((data) => data.calorie);
+    const user_exercise = await loggedInUserData.map((data) => data.exercise);
+    const user_sleep = await loggedInUserData.map((data) => data.sleep);
+    const user_water = await loggedInUserData.map((data) => data.water);
+    const sumUC = user_calorie.reduce((a, b) => a + b, 0);
+    const sumUE = user_exercise.reduce((a, b) => a + b, 0);
+    const sumUS = user_sleep.reduce((a, b) => a + b, 0);
+    const sumUW = user_water.reduce((a, b) => a + b, 0);
+    const avgUC = sumUC / user_calorie.length;
+    const avgUE = sumUE / user_exercise.length;
+    const avgUS = sumUS / user_sleep.length;
+    const avgUW = sumUW / user_water.length;
+    console.log("Average logged in user calories " + avgUC);
+    console.log("Average logged in user exercise " + avgUE);
+    console.log("Average logged in user sleep " + avgUS);
+    console.log("Average logged in user water " + avgUW);
+    // End - find average calories for User
+
+    // gather user data for ALL users
+    const allUserData = await Logs.findAll({
+      // order: [['day', 'DESC']],
+      // limit: 3,
+      attributes: ["calorie", "exercise", "sleep", "water"],
+    });
+    const allUsersUserData = await allUserData.map((post) =>
+      post.get({ plain: true })
+    );
+    console.log(allUsersUserData);
+    // END - gather user data for ALL users ^
+
+    // Find average calories for ALL users
+
+    // End - find average calories ALL users
 
     res.render("profile", {
-      userData,
+      loggedInUserData,
       // userData,
       logged_in: req.session.logged_in,
     });
